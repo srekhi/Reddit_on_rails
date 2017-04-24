40ef2ffabb7ee83fca5189320b032f3dde99cd02
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-04-24 10:11:33 -0700

    Initialize models/controllers

---
+++ reddit_on_rails/config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/create'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
